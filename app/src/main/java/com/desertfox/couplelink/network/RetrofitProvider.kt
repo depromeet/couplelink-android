2def0a446826c53fb8ef7ae0844f4813f82277a0
yunji <zion830@honglab.org>
2019-06-27 02:13:44 +0900

    채팅에 필요한 URL 분리

--- app/src/main/java/com/desertfox/couplelink/network/RetrofitProvider.kt
+++ app/src/main/java/com/desertfox/couplelink/network/RetrofitProvider.kt
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 val coupleLinkApi: Api by lazy {
     Retrofit.Builder()
-        .baseUrl("http://54.180.152.196:8080/")
+        .baseUrl(StompUrl.BASE_URL + "/")
         .client(provideOkHttpClient(provideLoggingInterceptor()))
         .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
         .addConverterFactory(GsonConverterFactory.create())
