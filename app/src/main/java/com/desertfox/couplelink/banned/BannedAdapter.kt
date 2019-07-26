470017cc598c41979ee20bdc0909acd5371661a0
jjy <skwntnftkgk@gmail.com>
2019-07-27 03:39:01 +0900

    금지어 개발 - 상대방 금지어 테스트 필요 메인화면 금지어 제외 준비중 토스트 띄움

--- app/src/main/java/com/desertfox/couplelink/banned/BannedAdapter.kt
+++ app/src/main/java/com/desertfox/couplelink/banned/BannedAdapter.kt
@@ -5,7 +5,17 @@ import androidx.recyclerview.widget.RecyclerView
 import com.desertfox.couplelink.BaseRecyclerViewHolder
 import com.desertfox.couplelink.model.responses.BannedTermModel
 
-class BannedAdapter : RecyclerView.Adapter<BaseRecyclerViewHolder>() {
+class BannedAdapter(private val callback: Callback) : RecyclerView.Adapter<BaseRecyclerViewHolder>() {
+
+    interface Callback {
+        fun addBanned(name: String)
+        fun deleteBanned(id: Int)
+    }
+
+    interface ContentCallback {
+        fun requestAddBanned(name: String)
+        fun requestDeleteBanned(id: Int)
+    }
 
     enum class ViewType {
         MY, PARTNER, ADD, HIDE
@@ -14,8 +24,20 @@ class BannedAdapter : RecyclerView.Adapter<BaseRecyclerViewHolder>() {
     private val items = mutableListOf<Item>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = when (viewType) {
-        ViewType.MY.ordinal, ViewType.PARTNER.ordinal -> BannedViewHolder(parent)
-        ViewType.ADD.ordinal -> BannedAddViewHolder(parent)
+        ViewType.MY.ordinal, ViewType.PARTNER.ordinal -> BannedViewHolder(parent, object : ContentCallback {
+            override fun requestAddBanned(name: String) {}
+
+            override fun requestDeleteBanned(id: Int) {
+                callback.deleteBanned(id)
+            }
+        })
+        ViewType.ADD.ordinal -> BannedAddViewHolder(parent, object : ContentCallback {
+            override fun requestAddBanned(name: String) {
+                callback.addBanned(name)
+            }
+
+            override fun requestDeleteBanned(id: Int) {}
+        })
         ViewType.HIDE.ordinal -> BannedHideViewHolder(parent)
         else -> throw IllegalArgumentException("Unknown viewType=$viewType")
     }
@@ -25,13 +47,14 @@ class BannedAdapter : RecyclerView.Adapter<BaseRecyclerViewHolder>() {
     override fun getItemCount() = items.size
     override fun getItemViewType(position: Int) = items[position].viewType.ordinal
 
-    fun appendData(model: BannedTermModel, isMy: Boolean) {
+    fun appendData(model: BannedTermModel) {
         val addIndex = items.size - 1
-        items.add(addIndex, Item(if (isMy) ViewType.MY else ViewType.PARTNER, model))
+        items.add(addIndex, Item(ViewType.MY, model))
         notifyItemInserted(addIndex)
     }
 
     fun setData(models: List<BannedTermModel>, isMy: Boolean) {
+        items.clear()
         items.addAll(models.map {
             if (isMy) Item(ViewType.MY, it)
             else {
--- app/src/main/java/com/desertfox/couplelink/banned/BannedViewHolder.kt
+++ app/src/main/java/com/desertfox/couplelink/banned/BannedViewHolder.kt
@@ -2,14 +2,25 @@ package com.desertfox.couplelink.banned
 
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import com.desertfox.couplelink.BaseRecyclerViewHolder
 import com.desertfox.couplelink.R
 import com.desertfox.couplelink.util.inflate
+import com.desertfox.couplelink.util.throttleClicks
+import kotlinx.android.synthetic.main.item_bannedterm.*
 
-class BannedViewHolder private constructor(itemView: View) : BaseRecyclerViewHolder(itemView) {
-    constructor(parent: ViewGroup) : this(parent.inflate(R.layout.item_bannedterm_hide, false))
+class BannedViewHolder private constructor(itemView: View,private val callback: BannedAdapter.ContentCallback) : BaseRecyclerViewHolder(itemView) {
+    constructor(parent: ViewGroup, callback: BannedAdapter.ContentCallback) : this(parent.inflate(R.layout.item_bannedterm, false), callback)
 
     override fun onDataBind(data: Any?) {
-
+        if (data !is BannedAdapter.Item) return
+        data.model?.apply {
+            item_bannedterm_count.text = this.count.toString()
+            item_bannedterm_txt.text = this.name
+            item_bannedterm_trash_btn.isVisible = data.viewType == BannedAdapter.ViewType.MY
+            item_bannedterm_trash_btn.throttleClicks().subscribe {
+                callback.requestDeleteBanned(id)
+            }.bind()
+        }
     }
 }
