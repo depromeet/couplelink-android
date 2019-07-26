c2bcbd15cc9e344ce55b47e541127b28882ee90e
yunji <zion830@naver.com>
2019-07-26 18:15:16 +0900

    RetrofitProvider conflict 수정

--- app/src/main/java/com/desertfox/couplelink/network/RetrofitProvider.kt
+++ app/src/main/java/com/desertfox/couplelink/network/RetrofitProvider.kt
@@ -1,33 +1,27 @@
 package com.desertfox.couplelink.network
 
+import android.content.Context
+import com.desertfox.couplelink.util.ACCESS_TOKEN
+import com.desertfox.couplelink.util.sharedPreferences
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RetrofitProvider constructor(context: Context) {
+class RetrofitProvider constructor(private val context: Context) {
 
     val coupleLinkApi: Api by lazy {
         Retrofit.Builder()
-            .baseUrl("http://54.180.152.196:8080/")
-            .client(provideOkHttpClient(provideLoggingInterceptor(), context))
+            .baseUrl(StompUrl.BASE_URL)
+            .client(provideOkHttpClient(provideLoggingInterceptor()))
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(Api::class.java)
     }
-val coupleLinkApi: Api by lazy {
-    Retrofit.Builder()
-        .baseUrl(StompUrl.BASE_URL)
-        .client(provideOkHttpClient(provideLoggingInterceptor()))
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-        .create(Api::class.java)
-}
 
-    private fun provideOkHttpClient(interceptor: HttpLoggingInterceptor, context: Context): OkHttpClient {
+    private fun provideOkHttpClient(interceptor: HttpLoggingInterceptor): OkHttpClient {
         val b = OkHttpClient.Builder()
         b.addInterceptor { chain ->
             val accessToken = context.sharedPreferences().getString(ACCESS_TOKEN, "").orEmpty()
@@ -40,10 +34,11 @@ val coupleLinkApi: Api by lazy {
         return b.build()
     }
 
-private fun provideLoggingInterceptor(): HttpLoggingInterceptor {
-    val interceptor = HttpLoggingInterceptor()
-    interceptor.apply {
-        level = HttpLoggingInterceptor.Level.BODY
+    private fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return interceptor
     }
-    return interceptor
 }
